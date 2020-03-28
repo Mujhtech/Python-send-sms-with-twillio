a56a6ce675fc80fb73faefa2579c9303b11d473f
Mujhtech <mujeeb.muhideen@gmail.com>
2020-03-28 02:44:08 +0100

    Add all files

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
-account_sid = "Your Account Sid"
-auth_token = "Your Auth Token"
-my_cell = "+Your Cell Number"
-my_twilio = "+Your Twilio Number"
+account_sid = "ACe4b05994ad4e49f9096822c498a9da63"
+auth_token = "ddd29406e75286388a16ffbc3b28cb36"
+my_cell = "+2347063148208"
+my_twilio = "+15127725551"
--- send_sms.py
+++ send_sms.py
@@ -1,10 +1,15 @@
-from twilio.rest import TwilioRestClient
+from twilio.rest import Client
 from credentials import account_sid, auth_token, my_cell, my_twilio
 
 # Find these values at https://twilio.com/user/account
-client = TwilioRestClient(account_sid, auth_token)
+client = Client(account_sid, auth_token)
 
-my_msg = "Your message goes here..."
+my_msg = "i LOVE YOU"
 
-message = client.messages.create(to=my_cell, from_=my_twilio,
-                                     body=my_msg)
+message = client.messages.create(
+         to=my_cell,
+         from_=my_twilio,
+         body=my_msg
+     )
+
+print(message.sid)
